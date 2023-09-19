40ef23cb5ed3d2aeaa563335b118f4e911b51ea0
Felix <taylo4ag@dukes.jmu.edu>
2023-09-19 15:02:01 -0400

    bug fix, comments

--- src/js/store.js
+++ src/js/store.js
@@ -14,12 +14,14 @@ const initialStore = {
 };
 
 function reducer(state, answer) {
+    // Skip the question: return the same state, but continue to the next question
     if (answer.type === Answer.IDK) {
         return {
             ...state,
             question: randomQuestion(state.question, state.pokemon),
         };
     }
+    // Restart the game: go to the default settings, but keep the "guessed" list the same
     if (answer.type === Answer.Restart) {
         return {
             ...state,
@@ -27,55 +29,56 @@ function reducer(state, answer) {
             question: randomQuestion(null, data),
         };
     }
+    // Did the user answer yes or no?
     let bool = answer.type === Answer.True ? true : false;
     var newPoke = state.pokemon;
     switch (state.question.current) {
-        case ("w"):
+        case ("w"): // Does the pokemon have wings?
             newPoke = state.pokemon.filter(p => p.wings === bool);
             break;
-        case ("m"):
+        case ("m"): // Does the pokemon have a mega-evolution?
             newPoke = state.pokemon.filter(p => p.mega === bool);
             break;
-        case ("l"):
+        case ("l"): // How many legs does it have?
             newPoke = state.pokemon.filter(p => (p.legs === state.question.param) === bool);
             break;
-        case ("c"):
+        case ("c"): // What color is it?
             newPoke = state.pokemon.filter(p => p.color.includes(state.question.param) === bool);
             break;
-        case ("t"):
+        case ("t"): // What type is it?
             newPoke = state.pokemon.filter(p => p.type.includes(state.question.param) === bool);
             break;
-        case ("b"):
+        case ("b"): // Is it a baby pokemon?
             newPoke = state.pokemon.filter(p => p.baby === bool);
             break;
-        case ("L"):
+        case ("L"): // Is it a legendary pokemon?
             newPoke = state.pokemon.filter(p => p.legendary === bool);
             break;
-        case ("r"):
+        case ("r"): // Does it have a regional form?
             newPoke = state.pokemon.filter(p => p.regional === bool);
             break;
-        case ("s"):
+        case ("s"): // Is it a starter pokemon?
             newPoke = state.pokemon.filter(p => p.starter === bool);
             break;
-        case ("f"):
+        case ("f"): // Is it a fossil pokemon?
             newPoke = state.pokemon.filter(p => p.fossil === bool);
             break;
-        case ("n"):
+        case ("n"): // What stage is it?
             newPoke = state.pokemon.filter(p => (p.stage === state.question.param) === bool);
             break;
-        case ("g"):
+        case ("g"): // What generation is it?
             newPoke = state.pokemon.filter(p => (p.gen === state.question.param) === bool);
             break;
-        case ("e"):
-            state.pokemon.filter(p => p.evolve.includes(state.question.param) === bool);
+        case ("e"): // How does it evolve?
+            newPoke = state.pokemon.filter(p => p.evolve.includes(state.question.param) === bool);
             break;
-        case ("p"):
+        case ("p"): // Is it x pokemon?
             if (bool) {
-                state.question.won = true;
+                state.question.won = true; // Won the game! Report how many guesses there have been
                 state.question.wonText = "Got it in " + state.question.count + " questions!";
                 if (!state.guessed.includes(state.question.param.name)) state.guessed.push(state.question.param.name);
                 return state;
-            } else {
+            } else { // Haven't won the game, continue asking
                 removeItem(newPoke, state.question.param.name);
                 state.question = randomQuestion(state.question, newPoke);
                 return {
@@ -83,25 +86,26 @@ function reducer(state, answer) {
                     pokemon: newPoke
                 };
             }
-        default:
+        default: // default settings
             return {
                 ...state,
                 question: randomQuestion(null, data)
             }
     }
-    console.log(newPoke);
     if (checkZero(state.question, newPoke)) {
-        return state;
+        return state; // If there are zero pokemon left, end the game
     }
+    // if the answer to the question was yes, there is no more need to ask that question in any form
     if (bool) removeItem(state.question.list, state.question.current);
     return {
         ...state,
         pokemon: newPoke,
-        question: randomQuestion(state.question, newPoke)
+        question: randomQuestion(state.question, newPoke) // pick a new question
     };
 }
 
 export function randomQuestion(q, p) {
+    // either use the existing question, or create a new default question
     var newQ = q || {
         "count": 0,
         "list": ["l", "c", "t", "n", "e", "g"],
@@ -127,14 +131,15 @@ export function randomQuestion(q, p) {
         "wonText": null,
         "aGens": [1, 2, 3]
     };
-    if (checkZero(newQ, p)) {
-        return newQ;
-    } if (newQ.list.length < 2 || p.length < 4) {
+    if (checkZero(newQ, p)) return newQ; // Zero pokemon left, end the game
+    if (newQ.list.length < 2 || p.length < 5) { 
+        // The list has been narrowed down, now guess which pokemon it is
         return guessPoke(newQ, p);
     }
     newQ.count++;
-    var randPoke = p[Math.floor(Math.random() * p.length)];
+    var randPoke = p[Math.floor(Math.random() * p.length)]; // select a random pokemon
     newQ.current = null;
+    // if a question hasn't been asked and the pokemon fulfills the condition, ask the question
     if (!newQ.specialMap.askWings && randPoke.wings) {
         editSpecial("askWings", "Does the Pokemon have wings?", newQ, "w");
     } else if (!newQ.specialMap.askLegendary && randPoke.legendary) {
@@ -144,21 +149,22 @@ export function randomQuestion(q, p) {
     } else if (!newQ.specialMap.askRegional && randPoke.regional) {
         editSpecial("askRegional", "Does the Pokemon have an alternate regional form?", newQ, "r");
     } else if (!newQ.specialMap.askMega && randPoke.mega) {
-        editSpecial("askMega", "Does the Pokemon have a mega-evolution?", newQ, "m");
+        editSpecial("askMega", "Does the Pokemon have a mega-evolution or primal form?", newQ, "m");
     } else if (!newQ.specialMap.askBaby && randPoke.baby) {
         editSpecial("askBaby", "Is it a baby Pokemon?", newQ, "b");
     } else if (!newQ.specialMap.askFossil && randPoke.fossil) {
         editSpecial("askFossil", "Is it a fossil Pokemon?", newQ, "f");
     } else do {
+        // otherwise select a random other question, but not "evolve" if the pokemon doesn't evolve
         newQ.current = newQ.list[Math.floor(Math.random() * newQ.list.length)];
     } while (newQ.current === "e" && randPoke.evolve.length === 0);
+    // proceed based on the typical question, if selected
     switch (newQ.current) {
         case ("l"):
             editTypical("Does it stand on "+ randPoke.legs + " legs/talons?", randPoke.legs, "aLegs", newQ, "l")
             break;
         case ("n"):
-            let stage = randPoke.color[Math.floor(Math.random() * randPoke.stage.length)];
-            editTypical("Is it a stage "+ stage + " Pokemon?", stage, "aStages", newQ, "n")
+            editTypical("Is it a stage "+ randPoke.stage + " Pokemon?", randPoke.stage, "aStages", newQ, "n")
             break;
         case ("g"):
             editTypical("Is it a gen "+ randPoke.gen + " Pokemon?", randPoke.gen, "aGens", newQ, "g")
@@ -175,12 +181,13 @@ export function randomQuestion(q, p) {
             let evolve = randPoke.evolve[Math.floor(Math.random() * randPoke.evolve.length)];
             editTypical("Does the Pokemon evolve by "+ evolve + "?", evolve, "aEvolve", newQ, "e");
             break;
-        default:
+        default: // a special question was selected, do nothing
             break;
     }
+    console.log(p);
+    console.log(randPoke);
     console.log(newQ.text);
     console.log(newQ.param);
-    console.log(randPoke);
     return newQ;
 }
 
--- src/js/util.js
+++ src/js/util.js
@@ -1,8 +1,13 @@
+// Some utilities for the store.js file, put in their own file for code cleanliness
+
+// remove a specific item from an array, if it exists
 export function removeItem(arr, item) {
     const index = arr.indexOf(item);
     if (index > -1) arr.splice(index, 1);
 }
 
+// if p.length is zero, end the game and return true
+// otherwise return false
 export function checkZero(q, p) {
     if (p.length === 0) {
         q.won = true;
@@ -11,6 +16,7 @@ export function checkZero(q, p) {
     } return false;
 }
 
+// guess a specific pokemon
 export function guessPoke(newQ, p) {
     newQ.count++;
     newQ.current = "p";
@@ -19,12 +25,14 @@ export function guessPoke(newQ, p) {
     return newQ;
 }
 
+// edit the question to a special question
 export function editSpecial(name, info, question, curr) {
     question.specialMap[name] = true;
     question.text = question.count + ". " + info;
     question.current = curr;
 }
 
+// edit the question to a typical question
 export function editTypical(info, pokeParam, qParam, q, curr) {
     q.param = pokeParam;
     q.text = q.count + ". " + info;
